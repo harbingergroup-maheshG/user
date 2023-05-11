1de74b44ee33f8962d6cc7779d62d819e933506c
hspl-maheshgujar <Mahesh.Gujar@harbingergroup.com>
2023-05-11 16:19:10 +0530

    Added User crud operation with respective UI pages

---
+++ src/main/java/com/myco/user/model/User.java
@@ -0,0 +1,88 @@
+package com.myco.user.model;
+
+
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Document(indexName = "userinformation_elasticsearch")
+public class User {
+	
+	@Id
+	private String id;
+	private String firstName;
+	private String lastName;
+	private long mobile;
+	private String email;
+	private Gender gender;
+	@Field(type = FieldType.Date, name = "created")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date created;
+	@Field(type = FieldType.Date, name = "updated")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updated;
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", mobile=" + mobile
+				+ ", email=" + email + ", gender=" + gender + ", created=" + created + ", updated=" + updated + "]";
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getUpdate() {
+		return updated;
+	}
+	public void setUpdate(Date updated) {
+		this.updated = updated;
+	}
+}
---
+++ src/main/java/com/myco/user/service/UserESServiceImpl.java
@@ -0,0 +1,81 @@
+package com.myco.user.service;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myco.user.model.User;
+import com.myco.user.repository.ElasticSearchQuery;
+
+@Service
+public class UserESServiceImpl implements UserESService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserESServiceImpl.class);
+	
+	@Autowired
+	private ElasticSearchQuery elasticSearchQuery;
+	
+	@Override
+	public List<User> getAllDocuments() {
+		LOGGER.info(">>>> getAllDocuments()");
+		try { 
+			List<User> users = elasticSearchQuery.searchAllDocuments();
+			
+			if(null != users && !users.isEmpty()) {
+				LOGGER.info("<<<< getAllDocuments(), Users : {}", users);
+				return users;
+			}
+		} catch (Exception e) {
+			LOGGER.error(">>>> error on  getAllDocuments() <<<< failed to fetch User record from elastic search, Exception : {} " , e.getMessage());
+			LOGGER.info(">>>> Calling record repair service");
+			// IN CASE OF FAILURE WE WILL BE FETCHING DATA FROM RECORD REPAIR SERVICE
+			
+		}
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String saveUser(User user) {
+		LOGGER.info(">>>> saveUser() , User : {}" , user);
+		try {
+			return elasticSearchQuery.createOrUpdateDocument(user);
+		} catch (Exception e) {
+			LOGGER.error(">>>> error on saveUser() <<<< Exception : {} " , e.getMessage());
+			LOGGER.info(">>>> calling record-repair-service >>>>");
+		}
+		return null;
+	}
+
+	@Override
+	public String deleteUser(String id) {
+		LOGGER.info(">>>> deleteUser() , id : {} " , id);
+		
+		try {
+			return  elasticSearchQuery.deleteDocumentById(id);
+		} catch (Exception e) {
+			LOGGER.error(">>>> error on deleteUser() <<<< Exception : {} " , e.getMessage());
+			return "Error while performing Delete operation! please try after sometime"; 
+		}
+	}
+
+	@Override
+	public User updateUser(String id) {
+		LOGGER.info(">>>> deleteUser() , id : {} " , id);
+		try {
+			User user =  elasticSearchQuery.getDocumentById(id);
+			if(null != user) {
+				LOGGER.info(">>>> updateUser , id : {}  ", id);
+				return user;
+			}
+		} catch (Exception e) {
+			LOGGER.error(">>>> error on deleteUser() <<<< Exception : {} " , e.getMessage());
+			
+		}
+		return null;
+	}
+	
+}
